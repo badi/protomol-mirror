237e6dd0b95d7ac159dd11292678cff009dd7fa0
jcoffland <jcoffland@ce46b6ef-3445-0410-911d-c47028e6f9df>
2008-02-01 04:35:58 +0000

    git-svn-id: https://protomol.svn.sourceforge.net/svnroot/protomol@614 ce46b6ef-3445-0410-911d-c47028e6f9df

---
+++ trunk/protomol-3.0/protomol/io/XYZTrajectoryReader.h
@@ -0,0 +1,78 @@
+/*  -*- c++ -*-  */
+#ifndef XYZTRAJECTORYREADER_H
+#define XYZTRAJECTORYREADER_H
+
+#include <protomol/io/Reader.h>
+#include <protomol/types/XYZ.h>
+
+namespace ProtoMol {
+  //____XYZTrajectoryReader
+
+  /**
+   * Reads a XYY trajectory files (ASCII).
+   */
+  class XYZTrajectoryReader : public Reader {
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Constructors, destructors (both default here), assignment
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  public:
+    XYZTrajectoryReader();
+    explicit XYZTrajectoryReader(const std::string &filename);
+    virtual ~XYZTrajectoryReader();
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // From class File
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  public:
+    virtual bool open() {myFirst = true; return File::open();}
+
+    virtual bool open(const std::string &filename) {
+      myFirst = true;
+      return File::open(filename);
+    }
+
+    virtual bool open(const char *filename) {
+      myFirst = true; return File::open(filename);
+    }
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // From class Reader
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  public:
+    virtual bool tryFormat();
+    virtual bool read();
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // New methods of class XYZ
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  public:
+    bool read(XYZ &xyz);
+    bool read(Vector3DBlock &coords, std::vector<std::string> &names);
+
+    XYZ getXYZ() const;
+    Vector3DBlock *orphanCoords();
+
+    std::vector<std::string> *orphanNames();
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Friends
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  public:
+    friend XYZTrajectoryReader &operator>>(XYZTrajectoryReader &xyzReader,
+                                           XYZ &xyz);
+
+    friend XYZTrajectoryReader &operator>>(XYZTrajectoryReader &xyzReader,
+                                           Vector3DBlock &coords);
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // My data members
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  private:
+    Vector3DBlock *myCoords;
+    std::vector<std::string> *myNames;
+    bool myFirst;
+  };
+
+  //____INLINES
+}
+#endif /* XYZTRAJECTORYREADER_H */
---
+++ trunk/protomol-3.0/protomol/io/XYZTrajectoryWriter.cpp
@@ -0,0 +1,200 @@
+#include <protomol/io/XYZTrajectoryWriter.h>
+
+#include <iomanip>
+
+#include <protomol/util/Report.h>
+#include <protomol/util/StringUtilities.h>
+
+using namespace std;
+using namespace ProtoMol::Report;
+using namespace ProtoMol;
+
+//____XYZTrajectoryWriter
+
+XYZTrajectoryWriter::XYZTrajectoryWriter() :
+  Writer(), myCoords(NULL), myNames(NULL), myAtoms(NULL), myAtomTypes(NULL),
+  myFirst(true) {}
+
+XYZTrajectoryWriter::XYZTrajectoryWriter(const string &filename) :
+  Writer(filename), myCoords(NULL), myNames(NULL), myAtoms(NULL),
+  myAtomTypes(NULL), myFirst(true) {}
+
+XYZTrajectoryWriter::~XYZTrajectoryWriter() {}
+
+bool XYZTrajectoryWriter::open(const XYZ &xyz) {
+  setNames(xyz.names);
+  myFirst = false;
+  return open();
+}
+
+bool XYZTrajectoryWriter::open(const vector<string> &names) {
+  setNames(names);
+  myFirst = false;
+  return open();
+}
+
+bool XYZTrajectoryWriter::open(const vector<Atom> &atoms,
+                               const vector<AtomType> &atomTypes) {
+  setNames(atoms, atomTypes);
+  myFirst = false;
+  return open();
+}
+
+bool XYZTrajectoryWriter::open(const string &filename, const XYZ &xyz) {
+  setNames(xyz.names);
+  myFirst = false;
+  return open(filename);
+}
+
+bool XYZTrajectoryWriter::open(const string &filename,
+                               const vector<string> &names) {
+  setNames(names);
+  myFirst = false;
+  return open(filename);
+}
+
+bool XYZTrajectoryWriter::open(const string &filename,
+                               const vector<Atom> &atoms,
+                               const vector<AtomType> &atomTypes) {
+  setNames(atoms, atomTypes);
+  myFirst = false;
+  return open(filename);
+}
+
+bool XYZTrajectoryWriter::reopen() {
+  if (myFirst) {
+    myFirst = false;
+    if (!open())
+      return false;
+  }
+
+  if (myFile.is_open())
+    close();
+
+  // Try to read the number of frames
+  myFile.clear();
+  myFile.open(myFilename.c_str(), ios::in);
+  string line, str;
+  line = getline();
+  close();
+  stringstream ss(line);
+  ss >> str;
+  if (toInt(str) > 0) {
+    // Ok, we have already written frames
+    str = (toString(toInt(str) + 1) + "                       ").substr(0, 19);
+    str += "\n";
+    myFile.clear();
+    myFile.open(myFilename.c_str(), ios::in | ios::out);
+    myFile.seekp(0, ios::beg);
+    myFile.write(str.c_str(), str.size());
+  } else {
+    // First time ...
+    myFile.clear();
+    myFile.open(myFilename.c_str(), ios::out | ios::trunc);
+    str = (toString(1) + "                       ").substr(0, 19);
+    str += "\n";
+    myFile << str;
+  }
+  close();
+  myFile.clear();
+  myFile.open(myFilename.c_str(), ios::out | ios::app);
+  return !myFile.fail();
+}
+
+bool XYZTrajectoryWriter::write(const XYZ &xyz) {
+  setCoords(xyz.coords);
+  setNames(xyz.names);
+  return write();
+}
+
+bool XYZTrajectoryWriter::write(const Vector3DBlock &coords) {
+  setCoords(coords);
+  return write();
+}
+
+bool XYZTrajectoryWriter::write(const Vector3DBlock &coords,
+                                const vector<string> &names) {
+  setNames(names);
+  setCoords(coords);
+  return write();
+}
+
+bool XYZTrajectoryWriter::write(const Vector3DBlock &coords,
+                                const vector<Atom> &atoms,
+                                const vector<AtomType> &atomTypes) {
+  setNames(atoms, atomTypes);
+  setCoords(coords);
+  return write();
+}
+
+bool XYZTrajectoryWriter::write() {
+  if (!reopen())
+    return false;
+  if (myCoords == NULL)
+    report << error << "[XYZTrajectoryWriter::write]"
+           << " No coordinates specified." << endr;
+  if (!(myNames != NULL || (myAtoms != NULL && myAtomTypes != NULL)))
+    report << error << "[XYZTrajectoryWriter::write]"
+           << " No atom names specified." << endr;
+  const unsigned int count = myCoords->size();
+  if ((myNames != NULL && myNames->size() != count) ||
+      (myAtoms != NULL && myAtomTypes != NULL && myAtoms->size() != count))
+    report << error << "[XYZTrajectoryWriter::write]"
+           << " Coorindate and atom name size are not equal." << endr;
+
+  // First, write the number of atoms
+  myFile << count << endl;
+
+  // Write atoms
+  myFile << setprecision(15);   // This should be some FLT_DIG or DBL_DIG ...
+  for (unsigned int i = 0; i < count; ++i) {
+    myFile <<
+    (myNames !=
+     NULL ? (*myNames)[i] : (*myAtomTypes)[(*myAtoms)[i].type].name) << "\t";
+    myFile.width(24);
+    myFile << (*myCoords)[i].x;
+    myFile.width(24);
+    myFile << (*myCoords)[i].y;
+    myFile.width(24);
+    myFile << (*myCoords)[i].z;
+    myFile << endl;
+  }
+
+  close();
+  return !myFile.fail();
+}
+
+void XYZTrajectoryWriter::setNames(const XYZ &xyz) {
+  myNames = &(xyz.names);
+  myAtoms = NULL;
+  myAtomTypes = NULL;
+}
+
+void XYZTrajectoryWriter::setNames(const vector<string> &names) {
+  myNames = &names;
+  myAtoms = NULL;
+  myAtomTypes = NULL;
+}
+
+void XYZTrajectoryWriter::setNames(const vector<Atom> &atoms,
+                                   const vector<AtomType> &atomTypes) {
+  myNames = NULL;
+  myAtoms = &atoms;
+  myAtomTypes = &atomTypes;
+}
+
+void XYZTrajectoryWriter::setCoords(const Vector3DBlock &coords) {
+  myCoords = &coords;
+}
+
+XYZTrajectoryWriter &operator<<(XYZTrajectoryWriter &xyzWriter,
+                                const XYZ &xyz) {
+  xyzWriter.write(xyz);
+  return xyzWriter;
+}
+
+XYZTrajectoryWriter &operator<<(XYZTrajectoryWriter &xyzWriter,
+                                const Vector3DBlock &coords) {
+  xyzWriter.write(coords);
+  return xyzWriter;
+}
